650efd5d26257566be81b4ca49b4b6449efc4b0d
KhalkarYash <yashmk2004@gmail.com>
2025-01-06 21:58:55 +0530

    Talk is cheap, show me the code!

--- App.js
+++ App.js
@@ -52,60 +52,55 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
-  const allRestaurants = restaurants.map((value, index) => {
+const RestaurantCard = (props) => {
+  const { resData } = props
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime
+  } = resData?.
     return (
       <div className="res-card">
         <img
           className="res-logo"
           src={
             "https://dineout-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/" +
-            value.cloudinaryImageId
+            cloudinaryImageId
           }
-          alt={value.name}
+          alt={name}
         />
-        <h3 className="resName">{value.name}</h3>
-        <h4 className="resData" id="resDataCuisines">{value.cuisines.join(", ")}</h4>
+        <h3 className="resName">{name}</h3>
+        <h4 className="resData" id="resDataCuisines">{cuisines.join(", ")}</h4>
         <div className="resDataContainer">
-          <h4 className="resData">{value.avgRating} ⭐</h4>
+          <h4 className="resData">{avgRating} ⭐</h4>
           <h4 className="resData">|</h4>
-          <h4 className="resData">{value.costForTwo}</h4>
+          <h4 className="resData">{costForTwo}</h4>
           <h4 className="resData">|</h4>
-          <h4 className="resData">{value.sla.deliveryTime + " mins"}</h4>
+          <h4 className="resData">{deliveryTime + " mins"}</h4>
         </div>
       </div>
     );
-  });
-
-  return <div className="restaurant-container">{allRestaurants}</div>;
-  // <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
 };
 
-// const RestaurantCard2 = (props) => {
-//   return (
-//     <div className="res-card">
-//       <img className="res-logo" src={props.url} alt={props.resName} />
-//       <h3>{props.resName}</h3>
-//       <h4>{props.cuisine}</h4>
-//       <h4>{props.rating} stars</h4>
-//       <h4>{props.distance}</h4>
-//     </div>
-//   );
-// };
+const Search = () => {
+  return (
+    <div></div>
+  )
+}
 
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="search"><Search /></div>
       <div className="restaurant-container">
-        <RestaurantCard />
-        {/* <RestaurantCard2
-          url="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=restaurant-food&sf=&txt_keyword=All"
-          resName="Yash"
-          cuisine="Indian"
-          rating="5"
-          distance="1.5"
-        /> */}
+        {
+          restaurants.map((restaurant)=>{
+            <RestaurantCard resData = {restaurant}/>
+          })
+        }
       </div>
     </div>
   );
