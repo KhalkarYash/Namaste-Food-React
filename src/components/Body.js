d3437f0663b450985adaddedcf718e2f632690af
KhalkarYash <yashmk2004@gmail.com>
2025-01-15 22:20:02 +0530

    hard coded Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,50 @@
 import RestaurantCard from "./RestaurantCard";
 import Search from "./Search";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+  // Local State Variable => Super powerful variable        (Hooks)
 
-// Local State Variable => Super powerful variable        (Hooks)
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  useEffect(() => {
+    console.log("useEffect() called");
+    fetchdata();
+  }, []);
 
-// Normal JS Variable
-// //  let listOfRestaurants = [];
-// listOfRestaurants = resList.map((resList) => {
-//   return resList;
-// });
+  const fetchdata = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.94727972810446&lng=73.83016818516963&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  // Normal JS Variable
+  // //  let listOfRestaurants = [];
+  // listOfRestaurants = resList.map((resList) => {
+  //   return resList;
+  // });
+
+  if (listOfRestaurants.length === 0) {
+    return (
+      <div>
+        <div className="search">
+        <div className="shimmerSearch"></div>
+      </div>
+      <div className="filter">
+        <div className="btnShimmer"></div>
+      </div>
+        <Shimmer />
+        <Shimmer />
+        <Shimmer />
+        <Shimmer />
+      </div>
+    );
+  }
 
   return (
     <div className="body">
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,8 +2,17 @@ import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
   const { resData } = props;
+  // console.log(resData)
   const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
-    resData;
+    resData.info;
+  var color;
+  if (avgRating>=4){
+    color = "green";
+  } else if(avgRating>3 && avgRating<4) {
+    color = "orange";
+  } else {
+    color = "red";
+  }
   return (
     <div className="res-card">
       <img
@@ -16,14 +25,16 @@ const RestaurantCard = (props) => {
       />
       <h3 className="resName">{name}</h3>
       <h4 className="resData" id="resDataCuisines">
-        {cuisines.join(", ")}
-      </h4>
+  {Array.isArray(cuisines) ? cuisines.join(", ") : "No cuisines available"}
+</h4>
       <div className="resDataContainer">
-        <h4 className="resData">{avgRating} ⭐</h4>
+        <div className="ratingColor" style={{backgroundColor:color}}>
+          <h4 className="resData ratingBG">{avgRating} <i class="fa-solid fa-star"></i></h4>
+        </div>
         <h4 className="resData">|</h4>
         <h4 className="resData">{costForTwo}</h4>
         <h4 className="resData">|</h4>
-        <h4 className="resData">{sla.deliveryTime + " mins"}</h4>
+        <h4 className="resData">{sla?.deliveryTime} mins</h4>
       </div>
     </div>
   );
