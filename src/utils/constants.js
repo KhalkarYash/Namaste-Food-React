ef0a3dffb6ca66c4e06db20f32f4a356e7b5d51e
KhalkarYash <yashmk2004@gmail.com>
2025-01-21 12:00:00 +0530

    Your commit message

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,8 @@ export const CDN_URL =
 export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWd8VwBrHSUVmPlRsqt9qibUCE26vZlIUvlw&s";
 
+export const itemImageURL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.9974533&lng=73.78980229999999&restaurantId=";
